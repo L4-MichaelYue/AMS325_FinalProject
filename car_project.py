9fcffdb5e8d154ba514952c20161b06845d6e8bd
yuxuanye1 <113309543+yuxuanye1@users.noreply.github.com>
2022-12-02 00:38:24 -0500

    Add files via upload

--- car_project.py
+++ car_project.py
@@ -9,6 +9,8 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 from collections import Counter
+import sys
+import tkinter as Tk
 
 def estimate(name,age,km_driven,fuel,seller_type,transmission,owner):
 
@@ -29,7 +31,8 @@ def estimate(name,age,km_driven,fuel,seller_type,transmission,owner):
     df_car['year'] = 2022 - df_car.year
 
     df = df_car[df_car['first'] == name]
-
+    if len(df) < 5:
+        return "Not enough data for this name"
     df_fuel = pd.get_dummies(df.fuel)
     df_seller = pd.get_dummies(df.seller_type)
     df_transmission = pd.get_dummies(df.transmission)
@@ -42,8 +45,6 @@ def estimate(name,age,km_driven,fuel,seller_type,transmission,owner):
     X = df_data
 
     y = np.log(df.selling_price)
-
-    # print(y)
 
     X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y,train_size = 0.7)
 
@@ -60,7 +61,136 @@ def estimate(name,age,km_driven,fuel,seller_type,transmission,owner):
                         }
     df2 = df2.append(dict,ignore_index=True)
     df2 = df2.fillna(0)
-    s = model.predict(df2)
-    return(np.exp(s))
+    if len(df2.columns) != len(X.columns):
+        return "Can't find this type"
+    s = np.exp(model.predict(df2))
+    return('The estimate value is %d' %s)
 
-print(estimate('Maruti Wagon R LXI Minor',15,50000,'Petrol','Individual','Manual','First Owner'))
+class Response:
+    def __init__(self, parent, main_obj):
+        self.parent = parent
+        self.main_obj = main_obj
+        
+        self.frame = Tk.Frame(self.parent)
+        self.frame.pack(fill=Tk.BOTH, expand=1)
+        
+        name = self.main_obj.field_name.get()
+        age = self.main_obj.field_age.get()
+        km_driven = self.main_obj.field_km_driven.get()
+        fuel = self.main_obj.field_fuel.get()
+        seller_type = self.main_obj.field_seller_type.get()
+        transmission = self.main_obj.field_transmission.get()
+        owner = self.main_obj.field_owner.get()
+        
+        result = estimate(name,age,km_driven,fuel,seller_type,transmission,owner)
+        
+        self.label = Tk.Label(self.frame, text=result)
+        self.label.grid(row=0, column=0, sticky=Tk.W)
+
+        self.frame_buttons = Tk.Frame(self.parent)
+        self.frame_buttons.pack(fill=Tk.BOTH, expand=1)
+        
+        self.ok_button = Tk.Button(self.frame_buttons, text="Ok", command=self._ok)
+        self.ok_button.pack(side=Tk.RIGHT, padx=5, pady=5)
+        
+    def _ok(self):
+        self.parent.destroy()
+
+class MyGUI2:
+    def __init__(self, parent):
+        self.parent = parent
+        #name
+        self.frame_name = Tk.Frame(self.parent)
+        self.frame_name.pack(fill=Tk.BOTH, expand=1)                                                                        
+        
+        self.label_name = Tk.Label(self.frame_name, text="name")
+        self.label_name.grid(row=0, column=0, sticky=Tk.W)
+        
+        self.label_str = Tk.StringVar()
+        
+        self.field_name = Tk.Entry(self.frame_name, textvariable=self.label_str)
+        self.field_name.grid(row=0, column=1, sticky=Tk.W)
+        #age
+        self.frame_age = Tk.Frame(self.parent)
+        self.frame_age.pack(fill=Tk.BOTH, expand=1)                                                                        
+        
+        self.label_age = Tk.Label(self.frame_age, text="age")
+        self.label_age.grid(row=1, column=0, sticky=Tk.W)
+        
+        self.label_str = Tk.StringVar()
+        
+        self.field_age = Tk.Entry(self.frame_age, textvariable=self.label_str)
+        self.field_age.grid(row=1, column=1, sticky=Tk.W)
+        #km_driven
+        self.frame_km_driven = Tk.Frame(self.parent)
+        self.frame_km_driven.pack(fill=Tk.BOTH, expand=1)                                                                        
+        
+        self.label_km_driven = Tk.Label(self.frame_km_driven, text="km_driven")
+        self.label_km_driven.grid(row=2, column=0, sticky=Tk.W)
+        
+        self.label_str = Tk.StringVar()
+        
+        self.field_km_driven = Tk.Entry(self.frame_km_driven, textvariable=self.label_str)
+        self.field_km_driven.grid(row=2, column=1, sticky=Tk.W)
+        #fuel
+        self.frame_fuel = Tk.Frame(self.parent)
+        self.frame_fuel.pack(fill=Tk.BOTH, expand=1)                                                                        
+        
+        self.label_fuel = Tk.Label(self.frame_fuel, text="fuel")
+        self.label_fuel.grid(row=3, column=0, sticky=Tk.W)
+        
+        self.label_str = Tk.StringVar()
+        
+        self.field_fuel = Tk.Entry(self.frame_fuel, textvariable=self.label_str)
+        self.field_fuel.grid(row=3, column=1, sticky=Tk.W)
+        #seller_type
+        self.frame_seller_type = Tk.Frame(self.parent)
+        self.frame_seller_type.pack(fill=Tk.BOTH, expand=1)                                                                        
+        
+        self.label_seller_type = Tk.Label(self.frame_seller_type, text="seller_type")
+        self.label_seller_type.grid(row=4, column=0, sticky=Tk.W)
+        
+        self.label_str = Tk.StringVar()
+        
+        self.field_seller_type = Tk.Entry(self.frame_seller_type, textvariable=self.label_str)
+        self.field_seller_type.grid(row=4, column=1, sticky=Tk.W)
+        #transmission
+        self.frame_transmission = Tk.Frame(self.parent)
+        self.frame_transmission.pack(fill=Tk.BOTH, expand=1)                                                                        
+        
+        self.label_transmission = Tk.Label(self.frame_transmission, text="transmission")
+        self.label_transmission.grid(row=5, column=0, sticky=Tk.W)
+        
+        self.label_str = Tk.StringVar()
+        
+        self.field_transmission = Tk.Entry(self.frame_transmission, textvariable=self.label_str)
+        self.field_transmission.grid(row=5, column=1, sticky=Tk.W)
+        #owner
+        self.frame_owner = Tk.Frame(self.parent)
+        self.frame_owner.pack(fill=Tk.BOTH, expand=1)                                                                        
+        
+        self.label_owner = Tk.Label(self.frame_owner, text="owner")
+        self.label_owner.grid(row=5, column=0, sticky=Tk.W)
+        
+        self.label_str = Tk.StringVar()
+        
+        self.field_owner = Tk.Entry(self.frame_owner, textvariable=self.label_str)
+        self.field_owner.grid(row=5, column=1, sticky=Tk.W)
+        
+        #bottom
+        self.frame_buttons = Tk.Frame(self.parent)
+        self.frame_buttons.pack(fill=Tk.BOTH, expand=1)
+
+        self.show_button = Tk.Button(self.frame_buttons, text="show", command=self._show)
+        self.show_button.pack(side=Tk.RIGHT, padx=5, pady=5)
+        
+    def _show(self):
+        self.new_window = Tk.Toplevel(self.parent)
+        self.show = Response(self.new_window, self)
+
+root = Tk.Tk()
+root.wm_title("Car prediction")
+
+gui_test = MyGUI2(root)
+
+root.mainloop()
